a198600251dc82e6cc56a82d97fd363f99544b99
learningmode <learningmode151@gmail.com>
2021-09-07 14:08:14 +0530

    Creatd comonents

---
+++ src/components/Greet.tsx
@@ -0,0 +1,13 @@
+type GreetProps ={
+    name:string,
+    messageCount:number,
+    isLoggedIn:true
+}
+export const Greet =(props:GreetProps)=>{
+    return(
+        <div>
+            <h2>{props.isLoggedIn?`Welcome {props.name}! you have {props.messageCount} unread messages`:
+                 `Welcome Guest`}</h2>
+        </div>
+    )
+}
