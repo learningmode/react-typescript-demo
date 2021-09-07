873077c8586ca959e82a5a79e251485d9f512ce3
learningmode <learningmode151@gmail.com>
2021-09-07 14:22:31 +0530

    corrected display

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -6,7 +6,7 @@ type GreetProps ={
 export const Greet =(props:GreetProps)=>{
     return(
         <div>
-            <h2>{props.isLoggedIn?`Welcome {props.name}! you have {props.messageCount} unread messages`:
+            <h2>{props.isLoggedIn?`Welcome ${props.name}! you have ${props.messageCount} unread messages`:
                  `Welcome Guest`}</h2>
         </div>
     )
